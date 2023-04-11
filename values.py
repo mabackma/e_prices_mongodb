01886806fae80b276b84477bd5d15671c91a6388
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-04-11 05:00:00 +0300

    importing data works. queries work with user input.

--- values.py
+++ values.py
@@ -3,42 +3,6 @@ import requests
 from datetime import datetime
 from pymongo import ASCENDING
 
-'''
-def import_values():
-    client = None
-
-    try:
-        client = pymongo.MongoClient('localhost', 27017)
-        db = client.prices
-
-        # Indeksoidaan kokoelma.
-        prices_collection = db.prices
-        prices_collection.create_index(
-            [('day', pymongo.ASCENDING), ('month', pymongo.ASCENDING), ('year', pymongo.ASCENDING),
-             ('hour', pymongo.ASCENDING)], unique=True)
-
-        res = requests.get('http://spotprices.energyecs.frostbit.fi/api/v1/prices')
-        prices_list = res.json()
-
-        for p in prices_list:
-            time_str = p['_time']
-            time_obj = datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%SZ')
-            day = time_obj.day
-            month = time_obj.month
-            year = time_obj.year
-            hour = time_obj.time().hour
-            price = {'day': day, 'month': month, 'year': year, 'hour': hour, 'value': p['value']}
-
-            # Lisätään arvot tietokantaan jos päivää ei löydy jo kokoelmasta.
-            db.prices.insert_one(price, {'upsert': True})
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        if client is not None:
-            client.close()
-'''
 
 def import_values():
     client = None
