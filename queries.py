7dc0f262ec79919c29d996827051ecd83f53180d
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-05-05 00:43:12 +0300

    importing data works. queries work with user input.

--- queries.py
+++ queries.py
@@ -13,74 +13,74 @@ def make_all_queries():
         user_month = int(user_date[5:7])
         user_day = int(user_date[8:10])
 
+        # Haetaan tuntikohtaiset hinnat valitulta päivältä.
         query = {'year': user_year, 'month': user_month, 'day': user_day}
         todays_prices = db.prices.find(query)
 
-        # Haetaan tuntikohtaiset hinnat valitulta päivältä.
         print(f"\nTuntikohtaiset hinnat päivälle {user_day}.{user_month}.{user_year}:")
         for price in todays_prices:
             print(f"{price['hour']}:00:   {round(price['value'], 3)} ¢")
 
-        query = {'year': user_year, 'month': user_month}
-        this_months_prices = list(db.prices.find(query))
+        # Haetaan päiväkohtaiset keskiarvohinnat valitulta kuukaudelta.
+        this_months_prices = db.prices.aggregate([
+            {
+                '$project': {
+                    '_id': 0
+                }
+            }, {
+                '$match': {
+                    'year': user_year,
+                    'month': user_month
+                }
+            }, {
+                '$group': {
+                    '_id': {
+                        'day': '$day'
+                    },
+                    'value': {
+                        '$avg': '$value'
+                    }
+                }
+            }, {
+                '$sort': {
+                    '_id.day': 1
+                }
+            }
+        ])
 
-        sum = 0
-        count = 0
-        index = 0
-        current_day = None
-
-        print(f"\nPäiväkohtaiset keskiarvohinnat kuukaudelle {user_year}-{user_month}:")
+        print(f"\nPäiväkohtaiset keskiarvohinnat kuukaudelle {user_month}:")
         for item in this_months_prices:
+            print(f"{item['_id']['day']}.{user_month}    {round(item['value'], 3)} ¢")
 
-            # Jos on kyseessä kuukauden viimeinen päivä niin lasketaan viimeisen päivän keskiarvo.
-            if index == len(this_months_prices) - 1:
-                sum += item['value']
-                count += 1
-                avg_price = round((sum / count), 3)
-                print(f"{item['day']}.{item['month']}:    {avg_price} ¢")
-
-            # Lasketaan aina edellisen päivän keskiarvo kun päivä vaihtuu.
-            if item['day'] != current_day:
-                if current_day is not None:
-                    avg_price = round((sum / count), 3)
-                    print(f"{item['day'] - 1}.{item['month']}:    {avg_price} ¢")
-                current_day = item['day']
-                sum = 0
-                count = 0
-            sum += item['value']
-            count += 1
-            index += 1
-
-        query = {'year': user_year}
-        this_years_prices = list(db.prices.find(query))
-
-        sum = 0
-        count = 0
-        index = 0
-        current_month = None
+        # Haetaan kuukausikohtaiset keskiarvohinnat valitulle vuodelle.
+        this_years_prices = db.prices.aggregate([
+            {
+                '$project': {
+                    '_id': 0
+                }
+            }, {
+                '$match': {
+                    'year': user_year
+                }
+            }, {
+                '$group': {
+                    '_id': {
+                        'month': '$month'
+                    },
+                    'value': {
+                        '$avg': '$value'
+                    }
+                }
+            }, {
+                '$sort': {
+                    '_id.month': 1
+                }
+            }
+        ])
 
         print(f"\nKuukausikohtaiset keskiarvohinnat vuodelle {user_year}:")
         for item in this_years_prices:
-
-            # Jos on kyseessä vuoden viimeinen päivä niin lasketaan viimeisen kuukauden keskiarvo.
-            if index == len(this_years_prices) - 1:
-                sum += item['value']
-                count += 1
-                avg_price = round((sum / count), 3)
-                print(f"{item['year']}-{item['month']}:    {avg_price} ¢")
-
-            # Lasketaan aina edellisen kuukauden keskiarvo kun kuukausi vaihtuu.
-            if item['month'] != current_month:
-                if current_month is not None:
-                    avg_price = round((sum / count), 3)
-                    print(f"{item['year']}-{item['month'] - 1}:    {avg_price} ¢")
-                current_month = item['month']
-                sum = 0
-                count = 0
-            sum += item['value']
-            count += 1
-            index += 1
-
+            print(f"{user_year}-{item['_id']['month']}    {round(item['value'], 3)} ¢")
 
     except Exception as e:
         print(e)
